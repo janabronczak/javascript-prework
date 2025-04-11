bdf4918423970b327a2c76fce296143d261c0212
Janina Brończak <janabronczak@gmail.com>
2025-04-11 18:19:20 +0200

    Remove var

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
-var buttonTest, buttonRock, buttonPaper, buttonScissors;
-
 /**
  * Main function triggered by button click
  */
 function buttonClicked(argButtonName) {
+    const playerMove = argButtonName,
+    randomNumber = Math.floor(Math.random() * 3 + 1),
+    computerMove = getMoveName(randomNumber);
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
-
-  var playerMove = argButtonName;
-  var randomNumber = Math.floor(Math.random() * 3 + 1);
-  console.log('wylosowana liczba to: ' + randomNumber);
-
-  var computerMove = getMoveName(randomNumber);
+  console.log('wylosowana liczba to: ' + randomNumber);  
   console.log('ruch komputera to: ' + computerMove);
 
   displayResult(playerMove, computerMove);
@@ -55,11 +51,11 @@ function displayResult(argPlayerMove, argComputerMove) {
 
 // Attach event listeners to buttons
 
-buttonRock = document.getElementById('button-rock');
+const buttonRock = document.getElementById('button-rock');
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
 
-buttonPaper = document.getElementById('button-paper');
+const buttonPaper = document.getElementById('button-paper');
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
 
-buttonScissors = document.getElementById('button-scissors');
+const buttonScissors = document.getElementById('button-scissors');
 buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
